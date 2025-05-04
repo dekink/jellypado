7a81742836abaa9032017f931dcfabf083c134a2
Daeun Kim <9190k@naver.com>
2025-05-04 18:52:24 +0900

    Rebase

---
+++ src/main/kotlin/com/example/jellypado/JellypadoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.jellypado
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JellypadoApplication
+
+fun main(args: Array<String>) {
+	runApplication<JellypadoApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/jellypado/JellypadoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.jellypado
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JellypadoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
